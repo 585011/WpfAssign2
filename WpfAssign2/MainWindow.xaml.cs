d3dad8dc89722a060cfabeda7594ca5ba0f3edd2
Martin Aas Eliassen <585011@stud.hvl.no>
2022-03-13 18:50:16 +0100

    task4

---
+++ WpfAssign2/MainWindow.xaml.cs
@@ -0,0 +1,202 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfAssign2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public static List<SpaceObject> solarSystem;
+        public static List<Moon> moons = new List<Moon>();
+        private double xOri;
+        private double yOri;
+        int time = 0;
+        public event Action<int> MoveIt;
+        SpaceObject Sun, Mercury, Venus, Earth, Mars, Jupiter, Saturn, Uranus, Neptune, Pluto;
+        public MainWindow()
+        {
+            InitializeComponent();
+            solarSystem = makeObjects();
+            makeMoons();
+
+            Loaded += delegate
+            {
+                xOri = WinScr.Width/2;
+                yOri = WinScr.Height/2;
+                createSolarS();
+            };
+
+        }
+        
+        private void T_Tick(Object sender, EventArgs e)
+        {
+            MoveIt(time++);
+        }
+        private List<SpaceObject> makeObjects()
+        {
+            List<SpaceObject> solarSystem = new List<SpaceObject>();
+            Sun = new Star("Sun", 0, 0.0, 696.34, 27.0, "Yellow");
+            Mercury = new Planet("Mercury", 57, 87.97, 2.439, 58.65, "LightGray");
+            Venus = new Planet("Venus", 108, 224.7, 6.052, 118.75, "LightGoldenrodYellow");
+            Earth = new Planet("Earth", 149, 365.26, 6.371, 23.93, "Blue");
+            Mars = new Planet("Mars", 227, 686.98, 3.389, 1.0, "Red");
+            Jupiter = new Planet("Jupiter", 778, 4332.71, 69.911, 0.414, "Beige");
+            Saturn = new Planet("Saturn", 1429, 10759.5, 58.232, 0.445, "Yellow");
+            Uranus = new Planet("Uranus", 2870, 30685.0, 25.362, 0.718, "LightBlue");
+            Neptune = new Planet("Neptune", 4504, 60190.0, 24.622, 0.67, "Azure");
+            Pluto = new DwarfPlanet("Pluto", 5913, 90550.0, 1.188, 6.39, "White");
+            
+            solarSystem.AddRange(new List<SpaceObject>
+        {
+            Sun, Earth, Mercury, Mars, Venus, Jupiter, Saturn, Uranus, Neptune, Pluto,
+            //new Asteroid("Asteroid belt", 478713, 1095.0, 308171, 0.0, "Gray")
+        });
+            return solarSystem;
+        }
+
+        private void makeMoons()
+        {
+            moons.AddRange(new List<Moon>
+            {
+            new Moon("The Moon", 384, 27.32, 1.737, 27.3,"White", Earth),//Earth
+            new Moon("Ganymede", 1070, 7.15, 2.634, 7.15, "White", Jupiter),//Jupiter
+            new Moon("Titan", 1222, 15.95, 2.574, 15.92, "Yellow", Saturn),//Saturn
+            new Moon("Triton", 355, -5.88, 1.353, 5.87, "Red", Neptune),//Neptune
+            new Moon("Titania", 436, 26146.0, 0.789, 8.7, "White", Uranus),//Uranus
+            new Moon("Phobos", 9, 0.32, 0.011, 0.32, "White", Mars),//Mars
+            new Moon("Nix", 49, 24.86, 0.019, 44.0, "White", Pluto),//pluto
+            });
+        }
+
+        public Ellipse drawObjects(Tuple<double,double> coord, int i)
+        {
+            double x = 0;
+            double y = 0;
+            Canvas c = new Canvas();
+
+            Color col = (Color)ColorConverter.ConvertFromString(solarSystem[i].color);
+            SolidColorBrush scb = new SolidColorBrush(col);
+
+            Ellipse ellip = new Ellipse()
+            {
+                
+                Name = solarSystem[i].name,
+                Fill = scb,
+                Width = 30*Math.Log(solarSystem[i].objectRadius),
+                Height = 30*Math.Log(solarSystem[i].objectRadius)
+            };
+            // To scale, use 30*log(objectRadius)
+            x = (10*Math.Log(coord.Item1)) + xOri - ellip.ActualWidth;
+            y = (10*Math.Log(coord.Item2)) + yOri - ellip.ActualHeight;
+            Canvas.SetLeft(ellip, x);
+            Canvas.SetTop(ellip, y);
+            
+
+            return ellip;
+
+        }
+
+        //public Ellipse drawOrbit (Tuple<double,double> coord, int i)
+        //{
+        //    double x = 0;
+        //    double y = 0;
+        //    Ellipse elOrb = new Ellipse()
+        //    {
+        //        Width = 30 * Math.Log(solarSystem[i].objectRadius),
+        //        Height = 30 * Math.Log(solarSystem[i].objectRadius),
+
+        //    };
+        //}
+
+        //public void moveObjects()
+        //{
+        //    for(int i = 0; i < solarSystem.Count; i++)
+        //    {
+        //        Ellipse ellip;
+        //    }
+        //}
+        public Tuple<double,double> calcPos(SpaceObject obj, int time)
+        {
+            return obj.calcPosition(time);
+        }
+        public static SpaceObject findParentObj(String name, List<SpaceObject> list)
+        {
+            SpaceObject parent = null;
+            foreach (SpaceObject o in list)
+            {
+                if (o.name.Equals(name))
+                {
+                    parent = o;
+                }
+            }
+            return parent;
+        }
+
+        public void createSolarS()
+        {
+            double x;
+            double y;
+            foreach (SpaceObject o in solarSystem)
+            {
+                if (o.name.Equals("Sun"))
+                {
+                    x = (calcPos(o, time).Item1) + (PCanvas.ActualWidth - (10*Math.Log(o.objectRadius)))-400;
+                    y = (calcPos(o, time).Item2) + (PCanvas.ActualHeight - (10*Math.Log(o.objectRadius)));
+
+
+                }
+                else
+                {
+                    x = (calcPos(o, time).Item1) + (PCanvas.ActualWidth - (10*Math.Log(o.objectRadius)))-450;
+                    y = (calcPos(o, time).Item2) + (PCanvas.ActualHeight - (10*Math.Log(o.objectRadius)));
+                }
+
+                Color col = (Color)ColorConverter.ConvertFromString(o.color);
+                SolidColorBrush scb = new SolidColorBrush(col);
+                
+                Ellipse ellip = new Ellipse()
+                {
+
+
+                    Name = o.name,
+                    Fill = scb,
+                    StrokeThickness = 1,
+                    Stroke = Brushes.Black,
+                    Width = 10*Math.Log(o.objectRadius),
+                    Height = 10*Math.Log(o.objectRadius)
+                };
+
+                Canvas.SetLeft(ellip, x);
+                Canvas.SetTop(ellip, y);
+
+                PCanvas.Children.Add(ellip);
+            }
+        }
+
+        public static String findParentPlanet(SpaceObject plan)
+        {
+            foreach (Moon moon in moons)
+            {
+                if (moon.parentPlanet.name == plan.name)
+                {
+                    return moon.ToString();
+                }
+            }
+            return "No parent";
+        }
+    }
+}
---
+++ WpfAssign2/spaceobj.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace WpfAssign2
+{
+    public class SpaceObject
+    {
+
+        public String name { get; set; }
+        public int orbitalRadius;
+        public double orbitalPeriod { get; set; }
+        public double objectRadius;
+        protected double rotationalPeriod;
+        public string color { get; set; }
+
+        public SpaceObject(String name, int orbitalRadius, double orbitalPeriod, double objectRadius, double rotationalPeriod, string color)
+        {
+            this.name = name;
+            this.orbitalRadius = orbitalRadius;
+            this.orbitalPeriod = orbitalPeriod;
+            this.objectRadius = objectRadius;
+            this.rotationalPeriod = rotationalPeriod;
+            this.color = color;
+
+        }
+        public virtual void Draw()
+        {
+            Console.WriteLine(name);
+            Console.WriteLine("Orbital radius: " + orbitalRadius);
+            Console.WriteLine("Orbital peroid: " + orbitalPeriod);
+            Console.WriteLine("Object radius: " + objectRadius);
+            Console.WriteLine("Rotational peroid: " + rotationalPeriod);
+            Console.WriteLine("Color: " + color);
+        }
+
+        public Tuple<double, double> calcPosition(double time)
+        {
+            // Use orbital radius and orbital period to calc pos relative to the planet's pos
+            double x;
+            double y;
+            double ts;
+            if (orbitalRadius == 0)
+            {
+                x = orbitalRadius; y = 1;
+                ts = 0.0;
+            }
+            else
+            {
+                double torb = time / orbitalPeriod;
+                double rad = 2 * Math.PI * torb;
+                ts = torb * 360;
+                // 2pi*(time/peroid) = radians
+                // x = radius*cos(t)
+                // x = radius * sin(Pi*2*angel/360) 
+                x = orbitalRadius * Math.Cos(rad);
+                y = orbitalRadius * Math.Sin(rad);
+            }
+            //double degs = Math.Atan2(y, x);
+            //Console.WriteLine("Degrees: " + String.Format("{0:0.00}°", ts));
+            return Tuple.Create(x, y);
+
+        }
+    }
+
+    public class Star : SpaceObject
+    {
+        public Star(String name, int orbitalRadius, double orbitalPeriod, double objectRadius, double rotationalPeriod, String color)
+            : base(name, orbitalRadius, orbitalPeriod, objectRadius, rotationalPeriod, color) { }
+        public override void Draw()
+        {
+            Console.Write("Star  : ");
+            base.Draw();
+        }
+    }
+
+    public class Planet : SpaceObject
+    {
+        public Planet(String name, int orbitalRadius, double orbitalPeriod, double objectRadius, double rotationalPeriod, String color)
+            : base(name, orbitalRadius, orbitalPeriod, objectRadius, rotationalPeriod, color) { }
+        public override void Draw()
+        {
+            Console.Write("Planet: ");
+            base.Draw();
+        }
+    }
+
+    public class Moon : SpaceObject
+    {
+        public SpaceObject parentPlanet { get; set; }
+        public Moon(String name, int orbitalRadius, double orbitalPeriod, double objectRadius, double rotationalPeriod, String color, SpaceObject parentPlanet)
+            : base(name, orbitalRadius, orbitalPeriod, objectRadius, rotationalPeriod, color) { this.parentPlanet = parentPlanet; }
+
+        public override void Draw()
+        {
+            Console.Write("Moon  : ");
+            base.Draw();
+            Console.WriteLine("Orbits: " + parentPlanet.name);
+        }
+
+        public override string ToString()
+        {
+            return ("Name: " + name + " / Orbit radius: " + orbitalRadius + " / Orbital period: " + orbitalPeriod
+                + " / Object radius: " + objectRadius + " / Rot.peroid: " + rotationalPeriod + " / Color: " + color);
+        }
+    }
+
+    public class Comet : SpaceObject
+    {
+        public Comet(string name, int orbitalRadius, double orbitalPeriod, double objectRadius, double rotationalPeriod, String color)
+            : base(name, orbitalRadius, orbitalPeriod, objectRadius, rotationalPeriod, color) { }
+
+        public override void Draw()
+        {
+            Console.Write("Comet  : ");
+            base.Draw();
+        }
+    }
+
+    public class Asteroid : SpaceObject
+    {
+        public Asteroid(string name, int orbitalRadius, double orbitalPeriod, double objectRadius, double rotationalPeriod, String color)
+            : base(name, orbitalRadius, orbitalPeriod, objectRadius, rotationalPeriod, color) { }
+
+        public override void Draw()
+        {
+            Console.Write("Asteroid  : ");
+            base.Draw();
+        }
+    }
+
+    public class AsteroidBelt : SpaceObject
+    {
+        public AsteroidBelt(string name, int orbitalRadius, double orbitalPeriod, double objectRadius, double rotationalPeriod, String color)
+            : base(name, orbitalRadius, orbitalPeriod, objectRadius, rotationalPeriod, color) { }
+
+        public override void Draw()
+        {
+            Console.Write("Asteroidbelt  : ");
+            base.Draw();
+        }
+    }
+
+    public class DwarfPlanet : Planet
+    {
+        public DwarfPlanet(String name, int orbitalRadius, double orbitalPeriod, double objectRadius, double rotationalPeriod, String color)
+            : base(name, orbitalRadius, orbitalPeriod, objectRadius, rotationalPeriod, color) { }
+
+        public override void Draw()
+        {
+            Console.Write("Dwarf planet  : ");
+            base.Draw();
+        }
+    }
+}
